cf303631af8925c33c8e010d56587bbc46034750
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-22 17:39:12 -0300

    fix: remove code

--- src/pages/Head/index.tsx
+++ src/pages/Head/index.tsx
@@ -1,5 +1,4 @@
-import { Home } from '../Home';
-import { MovieInfo } from '../MovieInfo';
+
 import './styles.css';
 
 export function Head() {
@@ -7,9 +6,6 @@ export function Head() {
     <section className="Head">
       <img className="" src="src/Assets/shuffle.svg" alt="Ícone de aleatório" />
       <h1>Não sabe o que assistir?</h1>
-      {/* <Home /> */}
-      <MovieInfo />
-      
     </section>
   );
 }
