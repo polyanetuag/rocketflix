5bb1d8668b8c1f857269e2d9e5a1fa900686530d
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-26 15:47:48 -0300

    fix: home component

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+import { Head } from "../../components/Head";
+import { Button } from "../../components/Button";
+
+export function Home() {
+  return (
+    <section className="Home">
+      <Head />
+      <Button />
+    </section>
+  );
+}
