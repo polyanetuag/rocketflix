b6b13c196b29cd5f1dce1dde3c2a303fc3c33961
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-26 15:48:22 -0300

    feat: routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MovieInfo from "./pages/MovieInfo";
+import { Home } from "./pages/Home";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<MovieInfo />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
