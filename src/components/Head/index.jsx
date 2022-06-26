516a76708e26a13f829d925a5f60b6f05c3d822d
Polyane Tuag <pollyportugues@hotmail.com>
2022-06-26 15:47:09 -0300

    fix: head component

---
+++ src/components/Head/index.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from "react-router-dom";
+import "./styles.css";
+
+export function Head() {
+  return (
+    <section className="Head">
+      <NavLink to="/">
+        <img
+          className=""
+          src="src/Assets/shuffle.svg"
+          alt="Ícone de aleatório"
+        />
+      </NavLink>
+      <h1>Não sabe o que assistir?</h1>
+    </section>
+  );
+}
