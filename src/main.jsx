6247e4d4ec105827289658bd99283a3c9e09a5eb
Polyane Tuag <pollyportugues@hotmail.com>
2022-07-16 08:56:24 -0300

    fix: deleted code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+// import App from "./App";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieInfo from "./pages/MovieInfo";
 import { Home } from "./pages/Home";
