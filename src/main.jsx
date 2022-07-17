1e95ea73d6bb104c293f5b6f04c84709c5c31658
Polyane Tuag <pollyportugues@hotmail.com>
2022-07-17 18:50:55 -0300

    fix: clean code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// import App from "./App";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MovieInfo from "./pages/MovieInfo";
